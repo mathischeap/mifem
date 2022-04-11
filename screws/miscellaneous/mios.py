385039af719b0cc83bf6120e270fefb0dbd8b612
Yi Zhang <zhangyi_aero@hotmail.com>
2022-04-11 13:22:16 +0800

    Changes

---
+++ screws/miscellaneous/mios.py
@@ -0,0 +1,22 @@
+"""make a dir"""
+
+
+from root.config.main import rAnk, mAster_rank
+import os
+
+def mkdir(folder_name):
+    """"""
+    if rAnk == mAster_rank:
+        if os.path.isdir(folder_name):
+            pass
+        else:
+            os.mkdir(folder_name)
+
+def remove(file_name):
+    if rAnk == mAster_rank:
+        os.remove(file_name)
+
+
+def rmdir(folder_name):
+    if rAnk == mAster_rank:
+        os.rmdir(folder_name)
